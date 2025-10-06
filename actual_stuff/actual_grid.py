46fa734281076dff6c6c65b12340249472b05528
lu-bian144 <bianchimluciana@gmail.com>
2025-10-06 18:49:13 -0300

    first draft f the sudoku grid yaaas

---
+++ actual_stuff/actual_grid.py
@@ -0,0 +1,23 @@
+from tkinter import*
+window = Tk()
+grid_frame = Frame(window, padx=300, pady=300)
+grid_frame.pack(expand=True)
+while True:
+    def get_input():
+        value = int(input(""))
+        return value
+
+    grid_number= int(get_input())
+
+    for i in range(grid_number):
+        cell_i= Entry(grid_frame, width= 2, font = "Arial 25")
+        cell_i.grid(row = i , column = 0, sticky='NSEW')
+        for j in range(grid_number):
+            cell_i= Entry(grid_frame, width= 2, font = "Arial 25")
+            cell_i.grid(row = i , column = j, sticky='NSEW')
+    for i in range(10):
+        grid_frame.grid_rowconfigure(i, weight=1)
+        grid_frame.grid_columnconfigure(i, weight=1)
+    grid_number=0
+
+window.mainloop()
