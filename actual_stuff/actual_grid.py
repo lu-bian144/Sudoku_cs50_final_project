f5442ea9d95e6a148c91295980a0e12413440d4a
lu-bian144 <bianchimluciana@gmail.com>
2025-10-06 23:48:12 -0300

    for some reason i accidentaly commited the last version without changing a single thing, but anyways this is the actual changed stuff. I still want to learn how to better the grid, and maybe ill try a canvas instead of a frame next time, but yeah ill go do my boring ecology report now lol

--- actual_stuff/actual_grid.py
+++ actual_stuff/actual_grid.py
@@ -1,22 +1,17 @@
 from tkinter import*
 window = Tk()
-grid_frame = Frame(window, padx=300, pady=300)
+grid_frame = Frame(window, width=500, height=300)
+grid_frame.propagate(False)
 grid_frame.pack(expand=True)
-while True:
-    def get_input():
-        value = int(input(""))
-        return value
+for i in range(9):
+    cell_i= Entry(grid_frame, width= 1, font = "Arial 30")
+    cell_i.grid(row = i , column = 0, sticky='NSEW')
+    for j in range(9):
+        cell_i= Entry(grid_frame, width= 1, font = "Arial 30")
+        cell_i.grid(row = i , column = j, sticky='NSEW')
+for i in range(10):
+    grid_frame.grid_rowconfigure(i, weight=1, minsize=70)
+    grid_frame.grid_columnconfigure(i, weight=1, minsize=70)
+grid_number=0
 
-    grid_number= int(get_input())
-
-    for i in range(grid_number):
-        cell_i= Entry(grid_frame, width= 2, font = "Arial 25")
-        cell_i.grid(row = i , column = 0, sticky='NSEW')
-        for j in range(grid_number):
-            cell_i= Entry(grid_frame, width= 2, font = "Arial 25")
-            cell_i.grid(row = i , column = j, sticky='NSEW')
-    for i in range(10):
-        grid_frame.grid_rowconfigure(i, weight=1)
-        grid_frame.grid_columnconfigure(i, weight=1)
-    grid_number=0
-
+window.mainloop()
