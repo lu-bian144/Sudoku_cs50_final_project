4f83cbbbb503d7a1a77085d9f91256e4cae00c06
lu-bian144 <bianchimluciana@gmail.com>
2025-10-09 01:29:00 -0300

    Just fixed some things about the sizing, and line highlights

--- actual_squarey_grid.py
+++ actual_squarey_grid.py
@@ -1,12 +1,12 @@
 from tkinter import *
 window = Tk()
 def main():
-    WIDTH = 362
-    HEIGHT = 362
+    WIDTH = 452
+    HEIGHT = 452
     grid = Canvas(window, width= WIDTH, height=HEIGHT)
     ancho = WIDTH-2
     height= HEIGHT-2
-    cell_size = 40
+    cell_size = 50
     big_square_outline = (cell_size)*3
     create_grid(ancho, height, cell_size, big_square_outline, grid)
 def create_grid(grid_width, grid_height, grid_cell_size, square_outline, canvas):
@@ -16,12 +16,12 @@ def create_grid(grid_width, grid_height, grid_cell_size, square_outline, canvas)
         for j in range (2, grid_height, grid_cell_size): # (starting value, finish value, skip of this many per iteration (0, 40, 80 ...))
             canvas.create_rectangle( i, j, i+grid_cell_size, j+grid_cell_size, width=1, outline= "black", fill="white")
     canvas.pack(expand=True)# line above: coordinate of the upper left corner (ithinkso), coordinates of bottom right corner
-    while l < 3:
+    while l < 10:
         canvas.create_line((square_outline*l)+2, 2, (square_outline*l)+2, grid_height+2, width=3, fill="blueviolet")
         canvas.create_line(2, (square_outline*l)+2, grid_height+2, (square_outline*l)+2, width=3, fill="blueviolet")
         l+=1
-    canvas.create_line(362,0,362,362, width=3, fill="blueviolet")
-    canvas.create_line(0,362,362,362,width=3, fill="blueviolet")
+    canvas.create_line(grid_width+2,0,grid_width+2,grid_height+2, width=3, fill="blueviolet")
+    canvas.create_line(0,grid_height+2,grid_width+2,grid_height+2,width=3, fill="blueviolet")
 main()
 window.mainloop()
 #if id [-1] == row/column number:
